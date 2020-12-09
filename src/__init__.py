d4c6cfbe9d33bfff97677ea74b98e663a7a41231
SpeedyCraftah <homodude55@gmail.com>
2020-12-09 18:56:00 +0000

    comment + beautify init file

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,5 @@
-import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+# This file is simply for the interpreter to know where to access
+# custom modules (in src folder).
+
+import os, sys
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
